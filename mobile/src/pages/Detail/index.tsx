b24c3b6175b343dbdb87180ee569571ef94bedb8
Rafael Aranda Martins <skema1114@hotmail.com>
2020-06-07 13:03:11 -0300

    Aula 04, checkpoint extra

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,15 +1,60 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, Image, SafeAreaView } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Detail = () => {
-  return <View />
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Text style={{ fontSize: 15, color: "#34cb79" }}>
+            {/* <Icon name="arrow-left" color="#34cb79" size={20} /> */}
+          Voltar
+        </Text>
+        </TouchableOpacity>
+
+        <Image
+          style={styles.pointImage}
+          source={{
+            uri: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+          }}
+        />
+
+        <Text style={styles.pointName}>Mercadão do Jõão</Text>
+        <Text style={styles.pointItems}>Lampadas, oleo de coxinha</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Rio do Sul, SC</Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => { }}>
+          {/* <FontAwesome name="whatsapp" size={20} color={#FFF} /> */}
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+        <RectButton style={styles.button} onPress={() => { }}>
+          {/* <Icon name="mail" size={20} color={#FFF} /> */}
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    paddingTop: 20,
   },
 
   pointImage: {
@@ -80,3 +125,8 @@ const styles = StyleSheet.create({
 });
 
 export default Detail;
+
+/**
+ * SAFEAREAVIEW do React-native, faz o padding automatico do limite de cima
+ *  e de baxo do celular
+ */
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Text, ScrollView, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text, ScrollView, Image, SafeAreaView } from 'react-native';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
+import api from '../../services/api'
+import * as Location from 'expo-location';
+
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
 
 const Points = () => {
   const navigation = useNavigation();
+  const [items, setItems] = useState<Item[]>([])
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
+
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data);
+    });
+  }, [])
+
 
   function handleNavigateBack() {
     navigation.goBack();
@@ -17,11 +34,22 @@ const Points = () => {
     navigation.navigate('Detail');
   }
 
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item == id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  }
+
   return (
     <>
       <View style={styles.container}>
         <TouchableOpacity onPress={handleNavigateBack}>
-          <Text style={{ fontSize: 30, color: "#34cb79" }}>
+          <Text style={{ fontSize: 15, color: "#34cb79" }}>
             {/* <Icon name="arrow-left" color="#34cb79" size={20} /> */}
           Voltar
         </Text>
@@ -64,35 +92,20 @@ const Points = () => {
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={{ paddingHorizontal: 20 }}
         >
-          <TouchableOpacity style={styles.item} onPress={() => { }}>
-            <SvgUri width={42} height={42} uri="http://192.168.1.202:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lampadas</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.item} onPress={() => { }}>
-            <SvgUri width={42} height={42} uri="http://192.168.1.202:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lampadas</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.item} onPress={() => { }}>
-            <SvgUri width={42} height={42} uri="http://192.168.1.202:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lampadas</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.item} onPress={() => { }}>
-            <SvgUri width={42} height={42} uri="http://192.168.1.202:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lampadas</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.item} onPress={() => { }}>
-            <SvgUri width={42} height={42} uri="http://192.168.1.202:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lampadas</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.item} onPress={() => { }}>
-            <SvgUri width={42} height={42} uri="http://192.168.1.202:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lampadas</Text>
-          </TouchableOpacity>
+          {items.map(item => (
+            <TouchableOpacity
+              style={[
+                styles.item,
+                selectedItems.includes(item.id) ? styles.selectedItem : {}
+              ]}
+              onPress={() => handleSelectItem(item.id)}
+              key={String(item.id)}
+              activeOpacity={0.5}
+            >
+              <SvgUri width={42} height={42} uri={item.image_url} />
+              <Text style={styles.itemTitle}>{item.title}</Text>
+            </TouchableOpacity>
+          ))}
         </ScrollView>
       </View>
     </>
@@ -201,4 +214,5 @@ export default Points;
 /**
  * SVGURI do pacote react-native-svg permite carregar um svg de uma url externa
  *
+ * O KEY DO MAP QUE PRECISA DE UM ID, TEM A NECESSIDADE DE QUE SEJA SEMPRE STRING
  */
