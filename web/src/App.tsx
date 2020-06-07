af34b9f4f46fee0c626520c55225e7aa8899b7b0
Rafael Aranda Martins <skema1114@hotmail.com>
2020-06-07 00:15:59 -0300

    Aula 03, finalizado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Routes />
   );
 }
 
 export default App;
+
+/**
+ * METODO ANTIGO DE ESCREVER
+ *  return React.createElement('h1', {
+ *    children: 'Hello World'
+ *  });
+ *
+ *
+ * JSX: sintaxe de XML dentro do Javascript
+ * tanto que a extensão do arquivo é TSX, ou seja, Typescript com JSX, se fosse só
+ *  TS, não seria permitido usar html aqui dentro no arquivo
+ *
+ * METODO NOVO DEPOIS DE ALGUMA ATUALIZAÇÃO
+ *  return (
+ *    <h1>Hello World</h1>
+ *  );
+ */
